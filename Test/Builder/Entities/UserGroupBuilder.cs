d821c1aba58f2bb0b761be024ed4d437090ca473
bruno <brunobentocaina@gmail.com>
2023-11-23 22:01:31 -0300

    feature: add test

---
+++ Test/Builder/Entities/UserGroupBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Bogus;
+using pkg_context.Entities;
+
+namespace Test.Builder.Entities;
+
+public class UserGroupBuilder
+{
+    private int? _maxUser;
+    private int _number;
+    private string _description;
+
+    public UserGroupBuilder()
+    {
+        var faker = new Faker();
+        _maxUser = faker.Random.Int(min: 1);
+        _description = "First group";
+        _number = faker.Random.Int(min: 1);
+    }
+
+    public static UserGroupBuilder Init() => new ();
+
+    public UserGroupBuilder SemDescription(string description)
+    {
+        _description = description;
+        return this;
+    }
+
+    public UserGroup Build() => new(_maxUser, _description, _number);
+    
+}
---
+++ Test/Test/Entities/UserGroupTest.cs
@@ -0,0 +1,15 @@
+﻿using Test.Builder.Entities;
+
+namespace Test.Test.Entities;
+
+public class UserGroupTest
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void NaoDeveCriarUserGroupSemDescricao(string description)
+    {
+        Assert.Throws<ArgumentException>(() => UserGroupBuilder.Init().SemDescription(description).Build());
+    }
+}
