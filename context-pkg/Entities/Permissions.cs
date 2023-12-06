c76e7dd7f1cc1d2cbd66274a9f5f5d83fdca1a03
Bruno <brunobento@prismafive.com.br>
2023-12-06 15:16:32 -0300

    .

--- context-pkg/Entities/Permissions.cs
+++ context-pkg/Entities/Permissions.cs
@@ -5,11 +5,11 @@ namespace pkg_context.Entities;
 public sealed class Permissions : BaseEntity
 {
     public Permissions(Guid id, DateTime created_at, DateTime update_at, bool active, int number,
-        bool isPremiu, string description) 
+        bool isPremiun, string description) 
         : base(id, created_at, update_at, active, number)
     {
         Description = description;
-        IsPremiun = isPremiu;
+        IsPremiun = isPremiun;
     }
 
     public bool IsPremiun { get; private set; }
